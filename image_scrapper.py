91e6e10a97d36ad9bf58127018406575ac84d535
Jerry <117933956+PerisicJere@users.noreply.github.com>
2023-11-19 23:01:10 -0500

    Update image_scrapper.py

--- image_scrapper.py
+++ image_scrapper.py
@@ -1,31 +1,41 @@
 '''
 Jere Perisic
-Scrapper for public art portland (https://www.publicartportland.org)
-Just run it and the images will download in directory
+Scraper for public art portland (https://www.publicartportland.org)
+Just run it, and the images and their links will be saved in the directory
 '''
 
 from bs4 import BeautifulSoup
 import requests
 import os
+from urllib.parse import urljoin
 
 directory = 'muse_images_folder'
+links_file = 'image_links.txt'
 
 
 def get_link_for_site():
     if not os.path.exists(directory):
         os.makedirs(directory)
+
     link = "https://www.publicartportland.org/"
     res = requests.get(link).text
     soup = BeautifulSoup(res, 'html.parser')
     find_div = soup.find('div', class_='isotope')
     images = find_div.select('div img')
-    for x in range(len(images)):
-        image_url = images[x]['src']
-        if "/uploads/" in image_url:
-            img_data = requests.get(image_url).content
-            img_name = str(image_url.split('/')[-1])
-            with open(directory + '/' + img_name, 'wb') as handler:
-                handler.write(img_data)
+
+    with open(links_file, 'w') as links_file_handle:
+        for x in range(len(images)):
+            image_url = images[x]['src']
+            if "/uploads/" in image_url:
+                absolute_url = urljoin(link, image_url)
+
+                img_data = requests.get(absolute_url).content
+                img_name = str(image_url.split('/')[-1])
+
+                with open(os.path.join(directory, img_name), 'wb') as handler:
+                    handler.write(img_data)
+
+                links_file_handle.write(f'{absolute_url}\n')
 
 
 if __name__ == "__main__":
