b60342edcb2ed015a26f762f1c252fdd1df57538
PerisicJere <jereperisic@icloud.com>
2023-12-02 16:24:32 -0500

    #6 folium map created

--- maps/visualization.py
+++ maps/visualization.py
@@ -1,27 +1,41 @@
+import json
+import mysql.connector
 import pandas as pd
 import folium
 from folium.plugins import MarkerCluster
 
+config_file = "../db_src/connectorConfig.json"
+with open(config_file, "r") as f:
+    config = json.load(f)
+    connection_config = config["mysql"]
+    conn = mysql.connector.connect(**connection_config)
+
 m = folium.Map(location=[43.6231, -70.4007], tiles='OpenStreetMap', zoom_start=12)
+cursor = conn.cursor()
 
-folium.Marker(location=[43.65694, -70.27833], popup="Hadlock Field Family Sculpture", icon=folium.Icon(color='blue')).add_to(m)
-folium.Marker(location=[43.652882, -70.266602], popup="Henry Wadsworth Longfellow Statue", icon=folium.Icon(color='blue')).add_to(m)
-folium.Marker(location=[43.657140,-70.258940], popup="Our Lady of Victories", icon=folium.Icon(color='blue')).add_to(m)
-folium.Marker(location=[43.65022426291442,-70.27626060655166], popup="Thomas Brackett “Czar” Reed", icon=folium.Icon(color='blue')).add_to(m)
-folium.Marker(location=[43.65756444769408, -70.25753617286682], popup="Michael", icon=folium.Icon(color='blue')).add_to(m)
-folium.Marker(location=[43.65871322509997, -70.27210937271118], popup="The Hiker, Spanish War Memorial", icon=folium.Icon(color='blue')).add_to(m)
-folium.Marker(location=[43.6473301, -70.3104787], popup="Playing Bears", icon=folium.Icon(color='blue')).add_to(m)
-folium.Marker(location=[43.6473301, -70.3103787], popup="A Spirit Of Its Own", icon=folium.Icon(color='blue')).add_to(m)
-folium.Marker(location=[43.6473301, -70.3102787], popup="Link", icon=folium.Icon(color='blue')).add_to(m)
-folium.Marker(location=[43.6473301, -70.3101787], popup="Acrobatic Dogs", icon=folium.Icon(color='blue')).add_to(m)
-folium.Marker(location=[43.6598936, -70.2465363], popup="Standing Bear", icon=folium.Icon(color='blue')).add_to(m)
-folium.Marker(location=[43.6583394, -70.1983239], popup="Lion's Head", icon=folium.Icon(color='blue')).add_to(m)
-folium.Marker(location=[43.6703594, -70.2842153], popup="Luminous Arbor", icon=folium.Icon(color='blue')).add_to(m)
-folium.Marker(location=[43.65871322509997, -70.27200937271118], popup="Great Black Hawk", icon=folium.Icon(color='blue')).add_to(m)
-folium.Marker(location=[43.6715641, -70.2965459], popup="Passing the Torch", icon=folium.Icon(color='blue')).add_to(m)
-folium.Marker(location=[43.65871322509997, -70.27230937271118], popup="Pinecone", icon=folium.Icon(color='blue')).add_to(m)
-folium.Marker(location=[43.654671, -70.257256], popup="John Ford Statue", icon=folium.Icon(color='blue')).add_to(m)
-folium.Marker(location=[43.6569416, -70.2561206], popup="Maine Lobsterman", icon=folium.Icon(color='blue')).add_to(m)
+cursor.execute("SELECT latitude, longitude, location_name, location_type FROM artLocation")
+locations = cursor.fetchall()
 
+conn.close()
+markerCluster = MarkerCluster().add_to(m)
+for location in locations:
+    lat, lon, popup, type = location
+
+    if lat is not None and lon is not None:
+        if type == 'Statue':
+            color = 'blue'
+        elif type == 'Sculpture':
+            color = 'red'
+        elif type == 'Music Venue':
+            color = 'green'
+        elif type == 'Museum':
+            color = 'gray'
+        elif type == 'Gallery':
+            color = 'purple'
+        elif type == 'Event Venue':
+            color = 'orange'
+        folium.Marker(location=[lat, lon], popup=popup, icon=folium.Icon(color=color)).add_to(markerCluster)
+    else:
+        print("Invalid latitude or longitude values. Skipping Marker creation.")
 
 m.save('index.html')
